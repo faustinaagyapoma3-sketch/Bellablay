fd8c4c569ca21793e7b9772363caf8d4483f1753
faustinaagyapoma3-sketch <faustinaagyapoma3@gmail.com>
2026-08-16 16:45:19 +0000

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = 'https://awonqlpigyujjhezmcho.supabase.co';
-export const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_UJpobmiJrhX_ukz66ODiEQ_F_FghhjV';
-export const OWNER_EMAIL = 'faustinaagyapoma3@gmail.com';
